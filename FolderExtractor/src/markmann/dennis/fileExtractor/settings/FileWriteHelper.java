0f9879680eec35b677c728c392f35e04f99876af
Dennis Markmann <dennismarkmann@hotmail.de>
2017-02-18 20:48:12 +0100

    Made settings files pretty / added some formatting.

--- FolderExtractor/src/markmann/dennis/fileExtractor/settings/FileWriteHelper.java
+++ FolderExtractor/src/markmann/dennis/fileExtractor/settings/FileWriteHelper.java
@@ -1,9 +1,15 @@
 package markmann.dennis.fileExtractor.settings;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -50,9 +56,17 @@ public class FileWriteHelper {
         final File file = new File(path + fileName);
 
         try {
-            TransformerFactory.newInstance().newTransformer().transform(new DOMSource(doc), new StreamResult(file));
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            FileOutputStream out = new FileOutputStream(file);
+            transformer.transform(new DOMSource(doc), new StreamResult(new OutputStreamWriter(out, "UTF-8")));
         }
-        catch (final TransformerException e) {
+        catch (final TransformerException | FileNotFoundException | UnsupportedEncodingException e) {
             LOGGER.error("Writing of '" + fileName + "' file failed.", e);
             e.printStackTrace();
         }
