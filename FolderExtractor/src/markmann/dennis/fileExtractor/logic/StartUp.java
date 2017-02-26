cc548aa4df0480b04c9aae18346ceefea2b39908
Dennis Markmann <dennismarkmann@hotmail.de>
2017-02-26 03:28:49 +0100

    Added some javadocs.

--- FolderExtractor/src/markmann/dennis/fileExtractor/logic/StartUp.java
+++ FolderExtractor/src/markmann/dennis/fileExtractor/logic/StartUp.java
@@ -4,10 +4,21 @@ import org.apache.log4j.Logger;
 
 import markmann.dennis.fileExtractor.logging.LogHandler;
 
+/**
+ * Class containing just the main method used to start the program.
+ *
+ * @author Dennis Markmann
+ *
+ */
 public class StartUp {
 
     private static final Logger LOGGER = LogHandler.getLogger("./Logs/FileExtractor.log");
 
+    /**
+     * Main method used to start the program.
+     *
+     * @param args
+     */
     public static void main(final String[] args) {
         LOGGER.info("Application starting.");
         Controller.startApplication();
