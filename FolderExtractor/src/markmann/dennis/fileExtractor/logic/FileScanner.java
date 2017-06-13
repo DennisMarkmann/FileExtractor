4c04bc6fe08b6653055f761d0e4686e8a0a88554
Dennis Markmann <dennismarkmann@hotmail.de>
2017-06-13 03:45:14 +0200

    Extracted popup logic to the controller.

--- FolderExtractor/src/markmann/dennis/fileExtractor/logic/FileScanner.java
+++ FolderExtractor/src/markmann/dennis/fileExtractor/logic/FileScanner.java
@@ -1,6 +1,5 @@
 package markmann.dennis.fileExtractor.logic;
 
-import java.awt.TrayIcon.MessageType;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,7 +13,6 @@ import markmann.dennis.fileExtractor.mediaObjects.Medium;
 import markmann.dennis.fileExtractor.settings.GeneralSettings;
 import markmann.dennis.fileExtractor.settings.SettingHandler;
 import markmann.dennis.fileExtractor.settings.TypeSettings;
-import markmann.dennis.fileExtractor.systemTray.SystemTrayMenu;
 
 /**
  * Used to scan for new media to process.
@@ -87,13 +85,13 @@ public class FileScanner implements Runnable {
         if (!this.isPathValid(extractionFolder)) {
             String errorMessage = "ExtractionFolder '" + extractionFolder.getAbsolutePath() + "' is not valid.";
             LOGGER.error(errorMessage);
-            this.showErrorNotification(errorMessage);
+            Controller.showErrorNotification(errorMessage);
             return;
         }
         if (!this.isPathValid(completionFolder)) {
             String errorMessage = "CompletionFolder '" + extractionFolder.getAbsolutePath() + "' is not valid.";
             LOGGER.error(errorMessage);
-            this.showErrorNotification(errorMessage);
+            Controller.showErrorNotification(errorMessage);
             return;
         }
         FileLister fl = new FileLister();
@@ -119,46 +117,8 @@ public class FileScanner implements Runnable {
             new HistoryHandler().addToHistory(mediaList);
         }
         if ((mediaList.size() > 0)) {
-            this.showExtractionNotification(mediaList);
+            Controller.showExtractionNotification(mediaList);
         }
     }
 
-    /**
-     * Shows an error notification popup in case the systemTray and popupNotification settings are enabled.
-     *
-     * @param errorMessage: The message shown in the popup.
-     */
-
-    private void showErrorNotification(String errorMessage) {
-        if (SettingHandler.getGeneralSettings().useSystemTray() && SettingHandler.getGeneralSettings().usePopupNotification()) {
-            SystemTrayMenu.sendTextPopup(errorMessage, MessageType.ERROR);
-        }
-    }
-
-    /**
-     * Showing a notification popup displaying the newest processed media file names.
-     *
-     * @param mediaList: List containing the information about the recently processed media files.
-     */
-
-    private void showExtractionNotification(ArrayList<Medium> mediaList) {
-        if (!SettingHandler.getGeneralSettings().useSystemTray()
-                || !SettingHandler.getGeneralSettings().usePopupNotification()) {
-            return;
-        }
-        StringBuilder fileNames = new StringBuilder();
-        int i = 0;
-        String infoString = "new file:";
-        for (Medium medium : mediaList) {
-            fileNames.append("\n");
-            fileNames.append(medium.getCompleteTitle());
-            i++;
-            // Cant show more than 2 entries with the windows 10 default popup anyway.
-            if (i == 2) {
-                infoString = mediaList.size() + " new files:";
-                break;
-            }
-        }
-        SystemTrayMenu.sendTextPopup("Extracted " + infoString + fileNames.toString(), MessageType.INFO);
-    }
 }
